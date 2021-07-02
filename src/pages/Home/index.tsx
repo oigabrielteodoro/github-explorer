44046b306f9433c3c035d8a36deca2c58e3dd83f
oigabrielteodoro <oi@gabrielteodoro.com>
2021-07-02 19:30:14 -0300

    refactor: deleted unsed vars

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,7 +1,5 @@
 import React, { useState, ChangeEvent, useEffect } from 'react'
 
-import { Link } from 'react-router-dom'
-
 import { RepositoryDTO } from '../../dtos/RepositoryDTO'
 
 import { Avatar } from '../../components/Avatar'
